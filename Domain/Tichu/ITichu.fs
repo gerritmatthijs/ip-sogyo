3ba948386289771720becb02eb6058d6750ed6b2
Gerrit Vos <gvos@sogyo.nl>
2024-01-30 13:52:02 +0100

    Add ITichu and ITichuFactory interfaces

---
+++ Domain/Tichu/ITichu.fs
@@ -0,0 +1,10 @@
+﻿namespace Tichu
+
+type ITichu = 
+    abstract member getPlayerHand: name: string -> string
+
+    abstract member hasTurn: name: string -> bool
+
+    abstract member getLastPlayed: unit -> string
+
+    abstract member doTurn: name: string * set: string -> ITichu
