b55e8abeb75e4a70b6654b5157f055ccc58a904a
Gerrit Vos <gvos@sogyo.nl>
2024-01-31 10:29:46 +0100

    Implement selecting the correct card from the image given a string

--- Client/src/components/card.tsx
+++ Client/src/components/card.tsx
@@ -1,7 +1,28 @@
 import { ChangeEventHandler } from "react";
+import { sendGreetings } from '../services/api.ts'
 
-export const Card = () => {
-    return <div className="card">
+type Props = {
+    card: String
+};
 
-    </div>
+export const Card = (props: Props) => {
+    const {card} = props;
+    const backgroundPos = getPosition(card);
+    console.log(backgroundPos);
+    return <button className="card" style={{'backgroundPosition': getPosition(card)}} onClick={() => sendGreetings()}>
+    </button>
+}
+
+function getPosition(card: String) {
+    switch(card) {
+        case "T": return "-900% 0%";
+        case "J": return "-1000% 0%";
+        case "Q": return "-1100% 0%";
+        case "K": return "-1200% 0%";
+        case "A": return "-1300% 0%";
+        default: 
+            const numberValue = Number(card);
+            const horizontalPosition = (numberValue - 1) * -100;
+            return horizontalPosition + "% 0%";
+    }
 }
--- Client/src/pages/App.tsx
+++ Client/src/pages/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import '../style/App.css'
 import '../style/card.css'
 import { sendGreetings } from '../services/api.ts'
+import { Card } from '../components/card.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,7 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <div>
-        <button className="card" onClick={() => sendGreetings()}>
-        </button>
+        <Card card="2"/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
