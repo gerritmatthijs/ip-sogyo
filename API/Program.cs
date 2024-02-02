52e59c5c5238e78c0be3ee556bd42a06094631eb
Gerrit Vos <gvos@sogyo.nl>
2024-02-02 07:48:51 +0100

    Test and implement getting last played card

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ITichuRepository>(new TichuRepositoryInMemory());
 builder.Services.AddSingleton<ITichuFactory>(new TichuFactory());
 
+// Needed to add info the the session
 builder.Services.AddDistributedMemoryCache();
 
 builder.Services.AddSession(options =>
--- Domain/Tichu.Test/TichuTests.fs
+++ Domain/Tichu.Test/TichuTests.fs
@@ -9,13 +9,13 @@ let ``Passing Test`` () =
 
 [<Fact>]
 let ``Tichu Creation`` () = 
-    let tichu = new TichuGame("2357TK") :> ITichu
+    let tichu = new TichuGame("2357TK", None) :> ITichu
     Assert.Equal("2357TK", tichu.GetPlayerHand("Gerrit"))
 
 [<Fact>]
 let ``Play single card removes it from hand`` () = 
     let handstring = "2346TTK"
-    let tichu = new TichuGame(handstring) :> ITichu
+    let tichu = new TichuGame(handstring, None) :> ITichu
     let newTichu = tichu.DoTurn("Gerrit", "T")
     // let hand = 
     //     match tryNewTichu with 
@@ -23,3 +23,11 @@ let ``Play single card removes it from hand`` () =
     //     | Error e -> handstring
     // Assert.True(Result.isOk tryNewTichu)
     Assert.Equal("2346TK", newTichu.GetPlayerHand("Gerrit"))
+
+[<Fact>]
+let ``Get last played card before and after a turn`` () = 
+    let handstring = "2346TTK"
+    let tichu = new TichuGame(handstring, None) :> ITichu
+    let newTichu = tichu.DoTurn("Gerrit", "T")
+    Assert.Equal("", tichu.GetLastPlayed())
+    Assert.Equal("T", newTichu.GetLastPlayed())
