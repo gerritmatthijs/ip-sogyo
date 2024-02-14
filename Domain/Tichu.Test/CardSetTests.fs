f66312b8dd40bfdc5b51eac2acc4801b4f3f2da7
Gerrit Vos <gvos@sogyo.nl>
2024-02-14 08:57:46 +0100

    Add subsequent pairs and remove double in Full House

--- Domain/Tichu.Test/CardSetTests.fs
+++ Domain/Tichu.Test/CardSetTests.fs
@@ -15,8 +15,8 @@ let ``Create Multiple CardSet`` () =
 let `` Create Full House CardSet`` () = 
     let fullHouseOne = "333QQ" |> StringToCardSet
     let fullHouseTwo = "66KKK" |> StringToCardSet
-    Assert.Equal(FullHouse(Card.Card('3'), Card.Card('Q')), fullHouseOne)
-    Assert.Equal(FullHouse(Card.Card('K'), Card.Card('6')), fullHouseTwo)
+    Assert.Equal(FullHouse(Card.Card('3')), fullHouseOne)
+    Assert.Equal(FullHouse(Card.Card('K')), fullHouseTwo)
 
 [<Fact>]
 let ``Create straight CardSet`` () = 
@@ -24,19 +24,35 @@ let ``Create straight CardSet`` () =
     Assert.Equal(Straight(Card.Card('8'), 5), straight)
 
 [<Fact>]
+let ``Create subsequent pairs CardSet`` () =
+    let doublePair = "99TT" |> StringToCardSet
+    let quadruplePair = "55667788" |> StringToCardSet
+    Assert.Equal(SubsequentPairs(Card.Card('9'), 2), doublePair)
+    Assert.Equal(SubsequentPairs(Card.Card('5'), 4), quadruplePair)
+
+[<Fact>]
 let ``Invalid sets yield a NonExistant type set`` () = 
     let invalidPair = "56" |> StringToCardSet
     let invalidFullHouse = "22225" |> StringToCardSet
     let invalidStraight = "5689T" |> StringToCardSet
+    let invalidSubsequentPairsOne = "556688" |> StringToCardSet
+    let invalidSubsequentPairsTwo = "566" |> StringToCardSet
     Assert.Equal(Invalid, invalidPair)
     Assert.Equal(Invalid, invalidFullHouse)
     Assert.Equal(Invalid, invalidStraight)
+    Assert.Equal(Invalid, invalidSubsequentPairsOne)
+    Assert.Equal(Invalid, invalidSubsequentPairsTwo)
 
 [<Fact>]
 let ``Multiples are recognised as the same type`` () = 
     let jackTriple = "JJJ" |> StringToCardSet
     let twoTriple = "222" |> StringToCardSet
     Assert.True(jackTriple |> CardSet.IsSameTypeAs(twoTriple))
+
+let ``Multipiles of different length are told apart`` () =
+    let jackDouble = "JJ" |> StringToCardSet
+    let twoTriple = "222" |> StringToCardSet
+    Assert.False(jackDouble |> CardSet.IsSameTypeAs(twoTriple))
 
 [<Fact>]
 let ``Full Houses are recognised as the same type`` () =
@@ -51,18 +67,33 @@ let ``Straights of the same length are recognised as the same type`` () =
     Assert.True(straightOne |> CardSet.IsSameTypeAs(straightTwo))
 
 [<Fact>]
-let ``CardSets of different types are told apart`` () = 
-    let jackDouble = "JJ" |> StringToCardSet
-    let twoTriple = "222" |> StringToCardSet
-    let fullHouse = "222JJ" |> StringToCardSet
-    Assert.False(jackDouble |> CardSet.IsSameTypeAs(twoTriple))
-    Assert.False(fullHouse |> CardSet.IsSameTypeAs(twoTriple))
-
-[<Fact>]
 let ``Straights of different length are told apart`` () =
     let straightOne = "34567" |> StringToCardSet
     let straightTwo = "56789T" |> StringToCardSet
     Assert.False(straightOne |> CardSet.IsSameTypeAs(straightTwo))
+
+[<Fact>]
+let ``Subsequent pairs of the same length are recognised as the same type`` () =
+    let SubsequentPairsOne = "223344" |> StringToCardSet
+    let SubsequentPairsTwo = "TTJJQQ" |> StringToCardSet
+    Assert.True(SubsequentPairsOne |> CardSet.IsSameTypeAs(SubsequentPairsTwo))
+
+[<Fact>]
+let ``Subsequent pairs of different lengths are told apart`` () = 
+    let subsequentPairsOne = "4455" |> StringToCardSet
+    let subsequentPairsTwo = "778899" |> StringToCardSet
+    Assert.False(subsequentPairsOne |> CardSet.IsSameTypeAs(subsequentPairsTwo))
+
+[<Fact>]
+let ``CardSets of different types are told apart`` () = 
+    let twoTriple = "222" |> StringToCardSet
+    let fullHouse = "222JJ" |> StringToCardSet
+    let straight = "45678" |> StringToCardSet
+    let subsequentPairs = "445566" |> StringToCardSet
+    Assert.False(fullHouse |> CardSet.IsSameTypeAs(twoTriple))
+    Assert.False(straight |> CardSet.IsSameTypeAs(fullHouse))
+    Assert.False(subsequentPairs |> CardSet.IsSameTypeAs(straight))
+    Assert.False(twoTriple |> CardSet.IsSameTypeAs(subsequentPairs))
 
 [<Fact>]
 let ``Higher Multiple is recognised as higher, but not conversely`` () = 
